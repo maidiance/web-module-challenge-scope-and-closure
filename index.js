66ce76c3658d86649e5432da06860714951f9ccb
Winifred Lu <winnie.lu@comcast.net>
2021-11-08 15:24:51 -0500

    task 2 and 3 complete

--- index.js
+++ index.js
@@ -28,11 +28,17 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+
+  counter1 uses a nested function in order to keep the variable count private whereas counter2 makes use of a global count variable.
   
   2. Which of the two uses a closure? How can you tell?
+
+  counter1: only counter1 has a nested function; only nested functions can use closures.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+     counter1 is better for tighter control over when the count variable can be updated; counter2 is better if the variable should be easily accessible.
 */
 
 // counter1 code
@@ -62,8 +68,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random() * 3);
 }
 
 
@@ -81,8 +87,17 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCb, innings){
+  let homeScore = 0;
+  let awayScore = 0;
+  for(let i = 0; i < innings; i++){
+    homeScore += inningCb();
+    awayScore += inningCb();
+  }
+  return {
+    Home: homeScore,
+    Away: awayScore,
+  };
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
